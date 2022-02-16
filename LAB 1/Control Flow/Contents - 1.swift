de823932d5fb2647d47b0fe3d531cd99df6c76df
Sharafitdin <mr.sharafitdin228@gmail.com>
2022-02-16 14:05:37 +0600

    sol

---
+++ LAB 1/Control Flow/Contents - 1.swift
@@ -0,0 +1,64 @@
+/*:
+ ## Exercise - Logical Operators
+
+ For each of the logical expressions below, print out what you think the resulting value will be (`true` or `false`). Then print out the actual expression to see if you were right. An example has been provided below.
+ 
+    43 == 53
+    print(false)
+    print(43 == 53)
+
+ 
+ 1. `9 == 9`
+ */
+
+print(true)
+
+/*:
+ 2. `9 != 9`
+ */
+
+print(false)
+
+/*: 
+ 3. `47 > 90`
+ */
+
+print(false)
+
+/*:
+ 4. `47 < 90`
+ */
+
+print(true)
+
+/*:
+ 5. `4 <= 4`
+ */
+
+print(true)
+
+/*:
+ 6. `4 >= 5`
+ */
+
+print(false)
+
+/*:
+ 7. `(47 > 90) && (47 < 90)`
+ */
+
+print(false)
+
+/*:
+ 8. `(47 > 90) || (47 < 90)`
+ */
+
+print(true)
+
+/*:
+ 9. `!true`
+ */
+
+print(false)
+
+//: page 1 of 9  |  [Next: Exercise - If and If-Else Statements](@next)
